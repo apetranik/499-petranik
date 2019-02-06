ea46a4fed4b355e2044cdcfe417cb1e0ef8af9f4
Aliya Petranik <petranik@usc.edu>
2019-02-05 18:19:50 -0800

    Connected Service layer to Key Value Store implementation thru GRPC

---
+++ backend_client.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+#include "key_value_store.grpc.pb.h"
+
+// Client for service layer.
+// takes requests from service layer and dispatches to backend_server thru GRPC
+class BackendClient {
+ public:
+   // constructs a Backend Client object that takes in grpc info
+   BackendClient(std::shared_ptr<grpc::Channel> channel) : stub_(chirp::KeyValueStore::NewStub(channel)) {}
+   // Constructs a payload to send to backend_server layer backend thru GRPC
+   // Will be received by get() in backend_server and return a vector of string as a reply
+   std::vector<std::string>* SendGetRequest(const std::vector<std::string> &keys);
+   // Constructs a payload to send to backend_server layer backend thru GRPC
+   // Will be received by put() in backend_server
+   // returns true if PUT was successful.
+   bool SendPutRequest(const std::string &key, const std::string &value);
+   // Constructs a payload to send to backend_server layer backend thru GRPC
+   // Will be received by del() in backend_server
+   // returns true if DELKEY was successful
+   bool SendDeleteKeyRequest(const std::string &key);
+
+ private:
+  // pointer used to communicate with service layer
+  std::unique_ptr<chirp::KeyValueStore::Stub> stub_;
+};
---
+++ backend_server.cc
@@ -0,0 +1,82 @@
+#include "backend_server.h"
+
+#include <map>
+#include <string>
+#include <utility>
+
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+#include "key_value_store.h"
+#include "key_value_store.grpc.pb.h"
+
+grpc::Status KeyValueStoreImpl::put(grpc::ServerContext *context, const chirp::PutRequest *request, chirp::PutReply *reply) {
+  if (request == nullptr) {
+    return grpc::Status(grpc::INVALID_ARGUMENT,"`PutRequest` is a nullptr","");
+  }
+
+  bool success = key_value_store_.Put(request->key(), request->value());
+
+  if (success) {
+    return grpc::Status::OK;
+  }
+  else {
+    return grpc::Status(grpc::UNKNOWN, "Unknown error", "");
+  }
+}
+
+grpc::Status KeyValueStoreImpl::get(grpc::ServerContext *context, grpc::ServerReaderWriter<chirp::GetReply, chirp::GetRequest> *stream) {
+  chirp::GetRequest request;
+
+  while(stream->Read(&request)) {
+    chirp::GetReply reply;
+    std::string value;
+
+    bool success = key_value_store_.Get(request.key(), &value);
+    if (success) {
+      reply.set_value(value);
+    }
+    else {
+      reply.set_value("");
+    }
+    stream->Write(reply);
+  }
+
+  return grpc::Status::OK;
+}
+
+grpc::Status KeyValueStoreImpl::deletekey(grpc::ServerContext *context,const chirp::DeleteRequest *request, chirp::DeleteReply *reply) {
+  if (request == nullptr) {
+    return grpc::Status(grpc::INVALID_ARGUMENT, "`DeleteRequest` is a nullptr", "");
+  }
+
+  bool success = key_value_store_.Del(request->key());
+
+  if (success) {
+    return grpc::Status::OK;
+  }
+  else {
+    return grpc::Status(grpc::NOT_FOUND, "Unknown error", "");
+  }
+}
+
+void run_server() {
+  std::string server_address("0.0.0.0:50002");
+  KeyValueStoreImpl service;
+
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Backend Server is listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char **argv) {
+  run_server();
+
+  return 0;
+}
